00bb355316cbebb3e5fa2def8d57592a8fa086f1
Mazen Gamal <mazeng18@gmail.com>
2024-03-07 14:34:01 +0200

    create settings view

--- lib/presentation/screens/app_layout/app_layout.dart
+++ lib/presentation/screens/app_layout/app_layout.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_last_app/core/app_routes/app_routes.dart';
+import 'package:news_last_app/presentation/settings_view/settings_view.dart';
 
 import '../bottom_navigation_bar/manager/bottom_navigation_bar_cubit.dart';
 import '../bottom_navigation_bar/views/custom_bottom_nav_bar.dart';
@@ -14,7 +16,18 @@ class AppLayout extends StatelessWidget {
         BottomNavigationBarCubit cubit = BottomNavigationBarCubit.get(context);
         return Scaffold(
           appBar: AppBar(
+            centerTitle: true,
             title: Text(cubit.titles[cubit.currentIndex]),
+            leading: const Icon(Icons.notifications),
+            actions:  [
+              Padding(
+                padding: const EdgeInsets.only(left: 8),
+                child: IconButton(icon:const Icon(Icons.settings) ,onPressed:(){
+                  customPushNavigator(context, const SettingsView());
+                } ,   ),
+              )
+            ],
+
           ),
           body: cubit.views[cubit.currentIndex],
           bottomNavigationBar: const CustomBottomNavBar(),
