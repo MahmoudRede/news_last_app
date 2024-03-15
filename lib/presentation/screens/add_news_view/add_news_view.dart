ac8591e2b6479c3876556e319ca08f147171cb7b
Mazen Gamal <mazeng18@gmail.com>
2024-03-15 15:16:59 +0200

    files enhance

--- lib/presentation/screens/add_news_view/add_news_view.dart
+++ lib/presentation/screens/add_news_view/add_news_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_last_app/presentation/screens/add_news_view/widgets/add_news_body.dart';
-import 'package:news_last_app/presentation/screens/add_view/manager/news_cubit.dart';
+
+import '../../../business_logic/news_cubit/news_cubit.dart';
 
 class AddNewsView extends StatelessWidget {
   const AddNewsView({super.key});
