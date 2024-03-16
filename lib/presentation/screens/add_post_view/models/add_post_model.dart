dd83fd181897dc8cd3aebb00956f1926e4683df9
maher <maheradel451@gmail.com>
2024-03-16 14:37:22 +0200

    enhance

--- lib/presentation/screens/add_post_view/models/add_post_model.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-class AddPostModel {
-  final String image,title;
-  final VoidCallback? function;
-
-  const AddPostModel(this.function, {required this.image, required this.title});
-}
