bd1764006c552f87e9a346ea17dbc65c42323bd7
Mazen Gamal <mazeng18@gmail.com>
2024-03-27 23:42:20 +0200

    get ads done ولله الحمد

--- lib/presentation/screens/home_screen/widgets/tabs_body/news_body.dart
+++ lib/presentation/screens/home_screen/widgets/tabs_body/news_body.dart
@@ -4,6 +4,7 @@ import 'package:news_last_app/business_logic/ads_cubit/ads_cubit.dart';
 import 'package:news_last_app/presentation/screens/home_screen/widgets/news_item.dart';
 
 import '../../../../../business_logic/news_cubit/news_cubit.dart';
+import '../ads_items.dart';
 
 class NewsBody extends StatelessWidget {
   const NewsBody({super.key});
@@ -25,11 +26,42 @@ class NewsBody extends StatelessWidget {
                 : NewsItem(newsItemModel: newsItem);
           },
           separatorBuilder: (context, index) {
-            return const SizedBox();
-            /*if ((index + 1) % 2 == 0 && index != newsCubit.news.length - 1) {
+            // Check if the index is odd and not the last news item
+            if (index % 2 == 1 && index != newsCubit.news.length * 2 - 1) {
               return BlocBuilder<AdsCubit, AdsState>(
                 builder: (context, adsState) {
-                  final adsItem = adsCubit.ads[index ~/2];
+                  if (adsState is  GetAdsSuccessState) {
+                    // Adjusted index for ads
+                    final adsIndex = index ~/ 2;
+                    if (adsIndex < adsCubit.ads.length) {
+                      final adsItem = adsCubit.ads[adsIndex];
+                      bool hasImage = adsItem.image != null && adsItem.image!.isNotEmpty;
+                      return hasImage
+                          ? AdsItemWithImage(adsItemModel: adsItem)
+                          : AdsItem(adsItemModel: adsItem);
+                    }else {
+                      // Repeat the ads if adsIndex exceeds the number of ads
+                      final repeatedAdsIndex = adsIndex % adsCubit.ads.length;
+                      final repeatedAdsItem = adsCubit.ads[repeatedAdsIndex];
+                      bool hasImage = repeatedAdsItem.image != null && repeatedAdsItem.image!.isNotEmpty;
+                      return hasImage
+                          ? AdsItemWithImage(adsItemModel: repeatedAdsItem)
+                          : AdsItem(adsItemModel: repeatedAdsItem);
+                    }
+                  }
+                  // Return an empty SizedBox if ads data is not yet available or adsIndex is out of range
+                  return const SizedBox();
+                },
+              );
+            } else {
+              return const SizedBox(); // Otherwise, return an empty SizedBox
+            }
+          },
+          /*separatorBuilder: (context, index) {
+            if ((index + 1) % 2 == 0 && index != newsCubit.news.length - 1) {
+              return BlocBuilder<AdsCubit, AdsState>(
+                builder: (context, adsState) {
+                  final adsItem = adsCubit.ads[index +1];
                   bool hasImage = adsItem.image != null && adsItem.image!.isNotEmpty;
                   return hasImage
                       ? AdsItemWithImage(adsItemModel: adsItem)
@@ -38,11 +70,8 @@ class NewsBody extends StatelessWidget {
               );
             } else {
               return const SizedBox(); // Otherwise, return an empty SizedBox
-            }*/
-          }
-          ,
-
-
+            }
+          }*/
           itemCount: newsCubit.news.length,
         );
       },
